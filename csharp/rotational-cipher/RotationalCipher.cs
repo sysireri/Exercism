8f63345fc30458d55fc158043082563f5d4ff361
Eric Sirois <sysireri@gmail.com>
2020-08-17 12:00:55 -0400

    RotationalCipher

--- csharp/rotational-cipher/RotationalCipher.cs
+++ csharp/rotational-cipher/RotationalCipher.cs
@@ -5,13 +5,12 @@ public static class RotationalCipher
     public static string Rotate(string text, int shiftKey)
     {
         System.Text.StringBuilder objStringBuilder = new System.Text.StringBuilder();
-        int intCurrentAsciiCode = 0;
 
         foreach(char chrCurrentChar in text)
         {
             if(char.IsLetter(chrCurrentChar))
             {
-                intCurrentAsciiCode = (char.ToLower(chrCurrentChar) - 'a' + shiftKey) % 26;
+                int intCurrentAsciiCode = (char.ToLower(chrCurrentChar) - 'a' + shiftKey) % 26;
                 objStringBuilder.Append((char)(char.IsUpper(chrCurrentChar) ? (intCurrentAsciiCode + 'A') : (intCurrentAsciiCode + 'a')));
             }
             else
