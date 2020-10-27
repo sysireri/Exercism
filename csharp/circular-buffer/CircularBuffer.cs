0e3ff2e66b40fbd70a9ea4aa4c58f1057ef21cfb
Eric Sirois <sysireri@gmail.com>
2020-10-27 16:36:10 -0400

    CircularBuffer

--- csharp/circular-buffer/CircularBuffer.cs
+++ csharp/circular-buffer/CircularBuffer.cs
@@ -1,31 +1,48 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public class CircularBuffer<T>
 {
+    private Queue<T> queBuffer;
+    private int intCapacity;
+
     public CircularBuffer(int capacity)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        intCapacity = capacity;
+        queBuffer = new Queue<T>(capacity);
     }
 
     public T Read()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (queBuffer.Peek() == null)
+        {
+            throw new InvalidOperationException("The queue is empty, Cannot Read.");
+        }
+
+        return queBuffer.Dequeue();
     }
 
     public void Write(T value)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (queBuffer.Count >= intCapacity)
+        {
+            throw new InvalidOperationException("The queue is full, Cannot Write.");
+        }
+        queBuffer.Enqueue(value);
     }
 
     public void Overwrite(T value)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (queBuffer.Count >= intCapacity)
+        {
+            queBuffer.Dequeue();
+        }
+        queBuffer.Enqueue(value);
     }
 
     public void Clear()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        queBuffer.Clear();
     }
 }
-
 //submit C:\Users\eric\Exercism\csharp\circular-buffer\CircularBuffer.cs
