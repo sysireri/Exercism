571b4d77be18b1c45035641c7681b44fc2f08318
Eric Sirois <sysireri@gmail.com>
2020-12-14 15:05:03 -0500

    Checkin Tow_Fer

--- python/two-fer/two_fer.py
+++ python/two-fer/two_fer.py
@@ -1,9 +1,7 @@
-strStartString = "One for "
-strEndString = ", one for me."
 strDefaultName = "you"
 
 def two_fer(name = strDefaultName) -> str:
 
-    return strStartString + name + strEndString;
+    return f"One for {name}, one for me."
 
 # submit C:\Users\eric\Exercism\python\two-fer\two_fer.py
