1470f9a57a4faeb88d4977a0c9ad39432a76284a
Eric Sirois <sysireri@gmail.com>
2020-12-14 17:38:13 -0500

    PANGRAMM

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,4 +1,13 @@
-def is_pangram(sentence) -> bol:
-    pass
+def is_pangram(sentence) -> bool:
+    sentence = sentence.upper()
+    
+    strAlplabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    for ChrCurrent in strAlplabet:
+
+        if ChrCurrent not in sentence:
+            return False
+
+    return True
+
 
 # submit C:\Users\eric\Exercism\python\pangram\pangram.py
